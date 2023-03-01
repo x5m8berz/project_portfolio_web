aa85d1165b1df4c7d77d5b51e593cfe7c6e6d3f9
Hanrocky <hanrocky.halim@gmail.com>
2023-03-01 23:36:07 +0700

    Added skill section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience</h2>
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">{skills.map}</motion.div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
