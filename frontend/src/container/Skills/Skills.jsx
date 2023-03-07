efe3fc9562cf84ec1df601165998dc05e8283858
Hanrocky <hanrocky.halim@gmail.com>
2023-03-07 22:55:47 +0700

    Finishing skill & experience section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -7,6 +7,9 @@ import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
   useEffect(() => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
@@ -20,7 +23,23 @@ const Skills = () => {
     <>
       <h2 className="head-text">Skills & Experience</h2>
       <div className="app__skills-container">
-        <motion.div className="app__skills-list">{skills.map}</motion.div>
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skill-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} />
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   );
