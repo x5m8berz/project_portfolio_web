2281e39d91b16175817b0bc99ad90c8fac37da1b
Hanrocky <hanrocky.halim@gmail.com>
2023-03-11 23:31:07 +0700

    Fixed a bug

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -45,28 +45,37 @@ const Skills = () => {
           ))}
         </motion.div>
         <motion.div className="app__skills-exp">
-          {experience?.works?.map((work) => (
-            <>
-              <motion.div
-                whileInView={{ opacity: [0, 1] }}
-                transition={{ duration: 0.5 }}
-                className="app__skills-exp-work"
-                data-tip
-                data-for={work.name}
-                key={work.name}
-              >
-                <h4 className="bold-text">{work.name}</h4>
-                <p className="p-text">{work.company}</p>
+          {experience?.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
               </motion.div>
-              <ReactTooltip
-                id={work.name}
-                effect="solid"
-                arrowColor="#fff"
-                className="skills-tooltip"
-              >
-                {work.desc}
-              </ReactTooltip>
-            </>
+            </motion.div>
           ))}
         </motion.div>
       </div>
