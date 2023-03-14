e6f25a643c65a9fe451a8c43ee016f5d960108bb
Hanrocky <hanrocky.halim@gmail.com>
2023-03-15 00:13:35 +0700

    Finish Skill & Experience Section

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -58,7 +58,7 @@ const Skills = () => {
                       transition={{ duration: 0.5 }}
                       className="app__skills-exp-work"
                       data-tip
-                      data-for={work.name}
+                      data-tooltip-id={work.name}
                       key={work.name}
                     >
                       <h4 className="bold-text">{work.name}</h4>
